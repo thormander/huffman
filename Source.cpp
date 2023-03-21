d716de9e69231e633d6f3b13b1cdb387f658482e
Thor <71967190+thormander@users.noreply.github.com>
2023-03-20 20:24:59 -0500

    Initial commit: Boiler plate

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
 
+using namespace std;
+
+// Function to compress input file and write the compressed data to output file
+void compress(string input_file, string output_file) {
+    // Your code here
+}
+
+// Function to decompress input file and write the decompressed data to output file
+void decompress(string input_file, string output_file) {
+    // Your code here
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cout << "Usage: " << argv[0] << " -huff input_file output_file" << endl;
+        return 1;
+    }
+
+    string input_file = argv[2];
+    string output_file = argv[3];
+
+    if (string(argv[1]) == "-huff") {
+        compress(input_file, output_file);
+    } else {
+        cout << "Unknown option " << argv[1] << endl;
+        return 1;
+    }
+
+    return 0;
+}
